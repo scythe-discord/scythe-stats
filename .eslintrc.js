9e0bcf7d8ec7e7ef199c7570bb20d712a855d97c
Brady Shi <brady-shi@outlook.com>
2020-01-10 23:23:45 -0800

    Improve configuration for node linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,21 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:node/recommended-module",
-    "prettier/@typescript-eslint"
-  ]
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:node/recommended-module',
+    'prettier/@typescript-eslint'
+  ],
+  rules: {
+    'node/no-missing-import': [
+      'error',
+      {
+        resolvePaths: ['./src'],
+        tryExtensions: ['.js', '.ts', '.json', '.node']
+      }
+    ]
+  }
 };
