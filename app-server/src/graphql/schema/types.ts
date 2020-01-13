d62f259f95dbd1c926ac331e61e591501e31bf36
Brady Shi <brady-shi@outlook.com>
2020-01-13 01:48:11 -0800

    Add existing generated types

---
+++ app-server/src/graphql/schema/types.ts
@@ -0,0 +1,82 @@
+export type Maybe<T> = T | null;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string,
+  String: string,
+  Boolean: boolean,
+  Int: number,
+  Float: number,
+  Upload: any,
+};
+
+
+export enum CacheControlScope {
+  Public = 'PUBLIC',
+  Private = 'PRIVATE'
+}
+
+export type Faction = {
+   __typename?: 'Faction',
+  id: Scalars['Int'],
+  name: Scalars['String'],
+};
+
+export type Match = {
+   __typename?: 'Match',
+  id: Scalars['Int'],
+  date: Scalars['Int'],
+  numRounds: Scalars['Int'],
+  playerResults: Array<PlayerMatchResult>,
+};
+
+export type Mutation = {
+   __typename?: 'Mutation',
+  _empty?: Maybe<Scalars['String']>,
+  logMatch?: Maybe<Match>,
+};
+
+
+export type MutationLogMatchArgs = {
+  playerMatchResults: Array<PlayerMatchResultInput>
+};
+
+export type PlayerMat = {
+   __typename?: 'PlayerMat',
+  id: Scalars['Int'],
+  name: Scalars['String'],
+};
+
+export type PlayerMatchResult = {
+   __typename?: 'PlayerMatchResult',
+  displayName: Scalars['String'],
+  steamId?: Maybe<Scalars['String']>,
+  faction: Faction,
+  playerMat: PlayerMat,
+  coins: Scalars['Int'],
+};
+
+export type PlayerMatchResultInput = {
+  displayName: Scalars['String'],
+  steamId?: Maybe<Scalars['String']>,
+  faction: Scalars['String'],
+  playerMat: Scalars['String'],
+  coins: Scalars['Int'],
+};
+
+export type Query = {
+   __typename?: 'Query',
+  _empty?: Maybe<Scalars['String']>,
+  faction?: Maybe<Faction>,
+  playerMat?: Maybe<PlayerMat>,
+};
+
+
+export type QueryFactionArgs = {
+  name: Scalars['String']
+};
+
+
+export type QueryPlayerMatArgs = {
+  name: Scalars['String']
+};
+
