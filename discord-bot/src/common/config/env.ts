0032571cce3e102294c2f7527484e3978237bb89
Brady Shi <brady-shi@outlook.com>
2020-01-14 00:56:19 -0800

    Introduce graphql-request, add some barebones log requests to the server

--- discord-bot/src/common/config/env.ts
+++ discord-bot/src/common/config/env.ts
@@ -1,2 +1,5 @@
 export const BOT_TOKEN = process.env.BOT_TOKEN;
 export const GAME_LOG_PREFIX = '@Automa';
+
+export const GRAPHQL_SERVER_URL =
+  process.env.GRAPHQL_SERVER_URL || 'http://localhost:3000/graphql';
--- discord-bot/src/logger/index.ts
+++ discord-bot/src/logger/index.ts
@@ -1,8 +1,17 @@
 import { Message } from 'discord.js';
+import { request } from 'graphql-request';
 
-import { GAME_LOG_PREFIX } from '../common/config';
+import { GAME_LOG_PREFIX, GRAPHQL_SERVER_URL } from '../common/config';
 
 import { extractGameLog } from './extract';
+
+const LOG_MATCH_QUERY = `
+  mutation logMatch($playerMatchResults: [PlayerMatchResultInput!]!)  {
+    logMatch(playerMatchResults: $playerMatchResults) {
+      id
+    }
+  }
+`;
 
 export const handleLogRequest = async (message: Message): Promise<void> => {
   if (!message.content.startsWith(GAME_LOG_PREFIX)) {
@@ -16,5 +25,14 @@ export const handleLogRequest = async (message: Message): Promise<void> => {
       `Failed to parse match results with content ${message.content}`
     );
     return;
+  }
+
+  try {
+    const data = await request(GRAPHQL_SERVER_URL, LOG_MATCH_QUERY, {
+      playerMatchResults: playerMatchResults.playerScores
+    });
+    console.log(JSON.stringify(data, undefined, 2));
+  } catch (error) {
+    console.error(JSON.stringify(error, undefined, 2));
   }
 };
