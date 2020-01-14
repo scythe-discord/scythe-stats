ace759a6b7824ee240d51c5f134a6bff610e8843
Brady Shi <brady-shi@outlook.com>
2020-01-13 23:26:17 -0800

    Fix missed rename

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import { APP_SERVER_PORT } from './common/config';
 import { app } from './app';
 import { graphqlServer } from './graphql';
 
-graphqlServer.applyMiddleware({ app: appServer });
+graphqlServer.applyMiddleware({ app });
 
 app.listen(APP_SERVER_PORT, () => {
   console.log(`Starting scythe-stats app server on port ${APP_SERVER_PORT}`);
