ffdcc13aa4a9d120db1763af6394927964200557
Brady Shi <brady-shi@outlook.com>
2020-01-13 22:41:15 -0800

    Prefer more conventional Express app naming

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -1,3 +1,3 @@
 import express from 'express';
 
-export const appServer = express();
+export const app = express();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
 import { APP_SERVER_PORT } from './common/config';
-import { appServer } from './app';
+import { app } from './app';
 import { graphqlServer } from './graphql';
 
 graphqlServer.applyMiddleware({ app: appServer });
 
-appServer.listen(APP_SERVER_PORT, () => {
+app.listen(APP_SERVER_PORT, () => {
   console.log(`Starting scythe-stats app server on port ${APP_SERVER_PORT}`);
   console.log(
     `You can access the graphql server at http://localhost:${APP_SERVER_PORT}${graphqlServer.graphqlPath}`
