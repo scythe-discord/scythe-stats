c8fc1fc6508558224927116de41a88a010c51d6e
Brady Shi <brady-shi@outlook.com>
2020-01-16 02:15:07 -0800

    Include initial seed for official factions

---
+++ server/src/db/seeders/20200116094049-official-factions.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const OFFICIAL_FACTIONS = [
+  'Polania',
+  'Saxony',
+  'Crimean',
+  'Nordic',
+  'Rusviet',
+  'Albion',
+  'Togawa'
+];
+
+module.exports = {
+  up: queryInterface => {
+    return queryInterface.bulkInsert(
+      'Factions',
+      OFFICIAL_FACTIONS.map(name => ({
+        name,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }))
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(
+      'Factions',
+      { name: { [Sequelize.Op.in]: OFFICIAL_FACTIONS } },
+      {}
+    );
+  }
+};
