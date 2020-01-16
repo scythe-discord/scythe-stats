b3401a7a637697d03ae174cf83a39958988f02ec
Brady Shi <brady-shi@outlook.com>
2020-01-15 23:01:52 -0800

    Add sequelize with some example connection callbacks

--- server/src/common/config/env.ts
+++ server/src/common/config/env.ts
@@ -1,1 +1,7 @@
 export const APP_SERVER_PORT = process.env.APP_SERVER_PORT || 3000;
+
+export const DB_NAME = process.env.DB_NAME || 'scythe';
+export const DB_USERNAME = process.env.DB_USERNAME || 'postgres';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'postgres';
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_PORT = Number(process.env.DB_PORT) || 5432;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import { APP_SERVER_PORT } from './common/config';
 import { app } from './app';
 import { graphqlServer } from './graphql';
+import { sequelize } from './db';
 
 graphqlServer.applyMiddleware({ app });
 
@@ -10,3 +11,12 @@ app.listen(APP_SERVER_PORT, () => {
     `You can access the graphql server at http://localhost:${APP_SERVER_PORT}${graphqlServer.graphqlPath}`
   );
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Successfully connected to the database!');
+  })
+  .catch((err: Error) => {
+    console.error('Unable to connect to the database:', err);
+  });
