898efe824f7fd99748c07807c4985f96685ce7b0
Brady Shi <brady-shi@outlook.com>
2020-01-17 02:17:05 -0800

    Add seed for official player mats

---
+++ server/src/db/seeders/20200117101023-official-player-mats.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const OFFICIAL_PLAYER_MATS = [
+  'Industrial',
+  'Engineering',
+  'Patriotic',
+  'Mechanical',
+  'Agricultural',
+  'Militant',
+  'Innovative'
+];
+
+module.exports = {
+  up: queryInterface => {
+    return queryInterface.bulkInsert(
+      'PlayerMats',
+      OFFICIAL_PLAYER_MATS.map(name => ({
+        name,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }))
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(
+      'PlayerMats',
+      { name: { [Sequelize.Op.in]: OFFICIAL_PLAYER_MATS } },
+      {}
+    );
+  }
+};
