c00b762d94145c6690adf6ebe89892eff1aab3a0
Brady Shi <brady-shi@outlook.com>
2020-01-17 19:54:12 -0800

    Add missing model exports

--- server/src/db/models/index.ts
+++ server/src/db/models/index.ts
@@ -1,2 +1,4 @@
 export { default as Faction } from './faction';
 export { default as PlayerMat } from './player-mat';
+export { default as PlayerMatchResult } from './player-match-result';
+export { default as Match } from './match';
