2fe425ed476c45912b92114fcfbd733479671749
Brady Shi <brady-shi@outlook.com>
2020-01-17 21:41:49 -0800

    Add more stringent constraints on factions/player mats

--- server/src/db/migrations/20200116092317-create-faction.js
+++ server/src/db/migrations/20200116092317-create-faction.js
@@ -9,6 +9,8 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
+        unique: true,
+        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
