82bc3935d9a8e86dd73636a5622d9b46a1aeebda
Brady Shi <brady-shi@outlook.com>
2020-01-20 23:36:08 -0800

    Rename discord-bot references to bot, for QOL improvements

--- bot/src/common/config/env.ts
+++ bot/src/common/config/env.ts
@@ -1,4 +1,5 @@
-export const BOT_TOKEN = process.env.BOT_TOKEN;
+export const BOT_TOKEN =
+  'NjYzNjY5MzU0Nzg4OTQ1OTMz.Xh1V3Q.ybKzpGdM6FPgGCk1GVeuNt22THo';
 export const GAME_LOG_PREFIX = '@Automa';
 export const GUILD_ID = process.env.GUILD_ID || '';
 export const VANILLA_LOG_CHANNEL_ID = process.env.VANILLA_LOG_CHANNEL_ID || '';
