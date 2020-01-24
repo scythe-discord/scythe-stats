643f20427bc6b9a6c9b7fa3b03df6bcee8aea20d
Brady Shi <brady-shi@outlook.com>
2020-01-24 00:21:20 -0800

    Add index for entities

---
+++ api/src/db/entities/index.ts
@@ -0,0 +1,5 @@
+export { default as Faction } from './faction';
+export { default as PlayerMat } from './player-mat';
+export { default as Match } from './match';
+export { default as Player } from './player';
+export { default as PlayerMatchResult } from './player-match-result';
