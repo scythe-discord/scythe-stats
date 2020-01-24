eb64440ca4b787b2982c63173eab9487c574bb07
Brady Shi <brady-shi@outlook.com>
2020-01-24 01:13:02 -0800

    Add seeding to initial faction, player mat migrations

--- api/src/db/migrations/1579851479207-Faction.ts
+++ api/src/db/migrations/1579851479207-Faction.ts
@@ -1,14 +1,34 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+const OFFICIAL_FACTIONS = [
+  'Polania',
+  'Saxony',
+  'Crimean',
+  'Nordic',
+  'Rusviet',
+  'Albion',
+  'Togawa'
+];
 
 export class Faction1579851479207 implements MigrationInterface {
-    name = 'Faction1579851479207'
+  name = 'Faction1579851479207';
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`CREATE TABLE "faction" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "UQ_51b7d060b06c5b3ed9e3411f6bc" UNIQUE ("name"), CONSTRAINT "PK_5935637aa4ecd999ac0555ae5a6" PRIMARY KEY ("id"))`, undefined);
-    }
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `CREATE TABLE "faction" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "UQ_51b7d060b06c5b3ed9e3411f6bc" UNIQUE ("name"), CONSTRAINT "PK_5935637aa4ecd999ac0555ae5a6" PRIMARY KEY ("id"))`,
+      undefined
+    );
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`DROP TABLE "faction"`, undefined);
-    }
+    const factionValues = OFFICIAL_FACTIONS.map(name => `('${name}')`).join(
+      ','
+    );
 
+    await queryRunner.query(
+      `INSERT INTO faction (name) VALUES ${factionValues};`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`DROP TABLE "faction"`, undefined);
+  }
 }
--- api/src/db/migrations/1579851571966-PlayerMat.ts
+++ api/src/db/migrations/1579851571966-PlayerMat.ts
@@ -1,14 +1,34 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+const OFFICIAL_PLAYER_MATS = [
+  'Industrial',
+  'Engineering',
+  'Patriotic',
+  'Mechanical',
+  'Agricultural',
+  'Militant',
+  'Innovative'
+];
 
 export class PlayerMat1579851571966 implements MigrationInterface {
-    name = 'PlayerMat1579851571966'
+  name = 'PlayerMat1579851571966';
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`CREATE TABLE "player_mat" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "UQ_e609287fd07431532768faa6aa6" UNIQUE ("name"), CONSTRAINT "PK_e3f066bd824539623b5f4393707" PRIMARY KEY ("id"))`, undefined);
-    }
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `CREATE TABLE "player_mat" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "UQ_e609287fd07431532768faa6aa6" UNIQUE ("name"), CONSTRAINT "PK_e3f066bd824539623b5f4393707" PRIMARY KEY ("id"))`,
+      undefined
+    );
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`DROP TABLE "player_mat"`, undefined);
-    }
+    const playerMatValues = OFFICIAL_PLAYER_MATS.map(
+      name => `('${name}')`
+    ).join(',');
 
+    await queryRunner.query(
+      `INSERT INTO player_mat (name) VALUES ${playerMatValues};`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`DROP TABLE "player_mat"`, undefined);
+  }
 }
