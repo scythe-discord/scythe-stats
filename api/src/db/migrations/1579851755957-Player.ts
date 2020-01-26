5544fb834013dd088484cc1ec0b60b55c6f9ff68
Brady Shi <brady-shi@outlook.com>
2020-01-25 21:23:23 -0800

    Format migration files

--- api/src/db/migrations/1579851755957-Player.ts
+++ api/src/db/migrations/1579851755957-Player.ts
@@ -1,14 +1,16 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Player1579851755957 implements MigrationInterface {
-    name = 'Player1579851755957'
+  name = 'Player1579851755957';
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`CREATE TABLE "player" ("id" SERIAL NOT NULL, "displayName" character varying NOT NULL, "steamId" character varying, CONSTRAINT "PK_65edadc946a7faf4b638d5e8885" PRIMARY KEY ("id"))`, undefined);
-    }
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `CREATE TABLE "player" ("id" SERIAL NOT NULL, "displayName" character varying NOT NULL, "steamId" character varying, CONSTRAINT "PK_65edadc946a7faf4b638d5e8885" PRIMARY KEY ("id"))`,
+      undefined
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`DROP TABLE "player"`, undefined);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`DROP TABLE "player"`, undefined);
+  }
 }
--- api/src/db/migrations/1579861216776-PlayerUniqueDisplayNameWhenNoSteamId.ts
+++ api/src/db/migrations/1579861216776-PlayerUniqueDisplayNameWhenNoSteamId.ts
@@ -1,14 +1,20 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class PlayerUniqueDisplayNameWhenNoSteamId1579861216776 implements MigrationInterface {
-    name = 'PlayerUniqueDisplayNameWhenNoSteamId1579861216776'
+export class PlayerUniqueDisplayNameWhenNoSteamId1579861216776
+  implements MigrationInterface {
+  name = 'PlayerUniqueDisplayNameWhenNoSteamId1579861216776';
 
-    public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_432d5047bcf5ad1d10b8e3e414" ON "player" ("displayName") WHERE "steamId" IS NULL`, undefined);
-    }
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "IDX_432d5047bcf5ad1d10b8e3e414" ON "player" ("displayName") WHERE "steamId" IS NULL`,
+      undefined
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`DROP INDEX "IDX_432d5047bcf5ad1d10b8e3e414"`, undefined);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `DROP INDEX "IDX_432d5047bcf5ad1d10b8e3e414"`,
+      undefined
+    );
+  }
 }
