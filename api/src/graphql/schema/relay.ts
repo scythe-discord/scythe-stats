9b41999363c600a62491e53f93addbfa40a880d9
Brady Shi <brady-shi@outlook.com>
2020-02-01 18:00:59 -0800

    Add __resolveType for Node

--- api/src/graphql/schema/relay.ts
+++ api/src/graphql/schema/relay.ts
@@ -1,5 +1,8 @@
 import { gql } from 'apollo-server';
 
+import Schema from './codegen';
+import { fromGlobalId } from 'graphql-relay';
+
 export const typeDef = gql`
   interface Node {
     id: ID!
@@ -13,4 +16,14 @@ export const typeDef = gql`
   }
 `;
 
-export const resolvers = {};
+export const resolvers: Schema.Resolvers = {
+  // @ts-ignore: TS complains that the type string may not be a type of
+  // an actual Node, but having this actually happen in practice in unlikely
+  Node: {
+    __resolveType: (node) => {
+      const { type } = fromGlobalId(node.id);
+
+      return type;
+    }
+  }
+};
