6695096a351b3150a9a3ba1af08392913ae4ba87
Brady Shi <brady-shi@outlook.com>
2020-02-04 23:25:50 -0800

    Add barebones apollo-client support to the site

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -1,24 +1,40 @@
 import App from 'next/app';
 import Head from 'next/head';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import withApollo from 'next-with-apollo';
 import { Provider as StyletronProvider } from 'styletron-react';
 import { BaseProvider, LightTheme } from 'baseui';
 
 import { styletron, debug } from '../styletron';
 
-export default class Site extends App {
+interface Props {
+  apollo: ApolloClient<any>;
+}
+
+class Site extends App<Props> {
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, apollo } = this.props;
     return (
       <>
         <Head>
-          <title>Scythe Stats</title>
+          <title>Beloved Pacifist</title>
         </Head>
         <StyletronProvider value={styletron} debug={debug} debugAfterHydration>
           <BaseProvider theme={LightTheme}>
-            <Component {...pageProps} />
+            <ApolloProvider client={apollo}>
+              <Component {...pageProps} />
+            </ApolloProvider>
           </BaseProvider>
         </StyletronProvider>
       </>
     );
   }
 }
+
+export default withApollo(({ initialState }) => {
+  return new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    cache: new InMemoryCache().restore(initialState || {})
+  });
+})(Site);
