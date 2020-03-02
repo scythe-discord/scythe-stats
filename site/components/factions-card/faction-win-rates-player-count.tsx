33ff51c54183fe8723dcfd9403d9c12d7c00bee0
Brady Shi <brady-shi@outlook.com>
2020-03-01 22:57:30 -0800

    Add a plotted line for expected win rate

--- site/components/factions-card/faction-win-rates-player-count.tsx
+++ site/components/factions-card/faction-win-rates-player-count.tsx
@@ -1,4 +1,7 @@
 import { FunctionComponent } from 'react';
+import { useStyletron } from 'baseui';
+import { StatefulTooltip, PLACEMENT } from 'baseui/tooltip';
+import { Block } from 'baseui/block';
 import {
   LineChart,
   ResponsiveContainer,
@@ -6,10 +9,17 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Line
+  Line,
+  Legend,
+  LegendValueFormatter
 } from 'recharts';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfo } from '@fortawesome/free-solid-svg-icons';
 
 import GQL from '../../lib/graphql';
+
+const WIN_RATE_LINE_NAME = 'win rate';
+const EXPECTED_WIN_RATE_LINE_NAME = 'expected win rate';
 
 interface Props {
   faction: GQL.Faction;
@@ -18,24 +28,67 @@ interface Props {
 const FactionWinRatesByPlayerCount: FunctionComponent<Props> = ({
   faction
 }) => {
+  const [css] = useStyletron();
   const data = faction.statsByPlayerCount.map(
     ({ playerCount, totalMatches, totalWins }) => {
       const winRate = (100 * totalWins) / totalMatches;
 
       return {
         playerCount,
-        value: winRate
+        value: winRate,
+        basis: 100 / playerCount
       };
     }
   );
+
+  const renderLegendContent: LegendValueFormatter = value => {
+    if (value === EXPECTED_WIN_RATE_LINE_NAME) {
+      return (
+        <span
+          className={css({
+            display: 'inline-flex',
+            alignItems: 'center'
+          })}
+        >
+          {value}
+          <StatefulTooltip
+            placement={PLACEMENT.bottom}
+            content={() => (
+              <Block>
+                the expected win rate, assuming factions are equally balanced
+              </Block>
+            )}
+          >
+            <div
+              className={css({
+                display: 'inline-flex',
+                color: '#212121',
+                cursor: 'pointer',
+                margin: '0 7px'
+              })}
+            >
+              <FontAwesomeIcon icon={faInfo} size="xs" />
+            </div>
+          </StatefulTooltip>
+        </span>
+      );
+    }
+
+    return value;
+  };
 
   return (
     <ResponsiveContainer width="100%" height={300}>
       <LineChart data={data}>
         <Tooltip
-          formatter={value => {
+          formatter={(value, name, b) => {
+            console.log(value, b);
             const winRate = `${Number(value).toFixed(2)}%`;
-            return [winRate, 'Win Rate'];
+            const capitalizedName = name
+              .split(' ')
+              .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+              .join(' ');
+            return [winRate, capitalizedName];
           }}
           labelFormatter={(playerCount: number) => {
             return `${playerCount} Players`;
@@ -49,7 +102,15 @@ const FactionWinRatesByPlayerCount: FunctionComponent<Props> = ({
             return `${val}%`;
           }}
         />
-        <Line dataKey="value" />
+        <Legend formatter={renderLegendContent} />
+        <Line name={WIN_RATE_LINE_NAME} dataKey="value" strokeWidth={4} />
+        <Line
+          name={EXPECTED_WIN_RATE_LINE_NAME}
+          dataKey="basis"
+          type="basis"
+          dot={false}
+          stroke="#636e72"
+        />
       </LineChart>
     </ResponsiveContainer>
   );
