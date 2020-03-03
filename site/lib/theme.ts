1a1decaebdafc8d68c008e0372ac52464c949c97
Brady Shi <brady-shi@outlook.com>
2020-03-03 00:45:57 -0800

    Add custom breakpoints for the site's media queries

--- site/lib/theme.ts
+++ site/lib/theme.ts
@@ -2,11 +2,31 @@ import { createTheme, lightThemePrimitives } from 'baseui';
 
 export const PRIMARY_FONT_FAMILY = 'Fira Sans, Helvetica, sans-serif, serif';
 
+const breakpoints: { [key: string]: number } = {
+  medium: 750,
+  large: 1450
+};
+
+const responsiveThemeOverrides = Object.keys(breakpoints).reduce(
+  (acc, key) => {
+    acc.mediaQuery[
+      key
+    ] = `@media screen and (min-width: ${breakpoints[key]}px)`;
+    return acc;
+  },
+  {
+    breakpoints,
+    mediaQuery: {} as { [key: string]: string }
+  }
+);
+
 const primitives = {
   ...lightThemePrimitives,
   primaryFontFamily: PRIMARY_FONT_FAMILY
 };
 
-const overrides = {};
+const overrides = {
+  ...responsiveThemeOverrides
+};
 
 export default createTheme(primitives, overrides);
