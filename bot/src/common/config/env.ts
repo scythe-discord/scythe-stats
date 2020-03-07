abe3bd7cea20bc76e17365331df9661e948b9062
Brady Shi <brady-shi@outlook.com>
2020-03-07 00:47:14 -0800

    Remove hardcoded Discord bot values in favor of env vars

--- bot/src/common/config/env.ts
+++ bot/src/common/config/env.ts
@@ -1,9 +1,7 @@
-export const BOT_TOKEN =
-  'NjYzNjY5MzU0Nzg4OTQ1OTMz.Xh1V3Q.ybKzpGdM6FPgGCk1GVeuNt22THo';
-export const GAME_LOG_PREFIX = '<@&375734381022674946>';
-export const GUILD_ID = process.env.GUILD_ID || '299026961630494722';
-export const VANILLA_LOG_CHANNEL_ID =
-  process.env.VANILLA_LOG_CHANNEL_ID || '299320358215876609';
+export const BOT_TOKEN = process.env.BOT_TOKEN || '';
+export const GAME_LOG_PREFIX = process.env.GAME_LOG_PREFIX || '';
+export const GUILD_ID = process.env.GUILD_ID || '';
+export const VANILLA_LOG_CHANNEL_ID = process.env.VANILLA_LOG_CHANNEL_ID || '';
 
 export const GRAPHQL_SERVER_URL =
   process.env.GRAPHQL_SERVER_URL || 'http://localhost:4000/';
