a492dbf5b9a6811453db52e688a5519363ef975f
Brady Shi <brady-shi@outlook.com>
2020-04-21 20:58:12 -0700

    Opt for more consistent URL joining for paths

--- site/lib/scythe/factions.ts
+++ site/lib/scythe/factions.ts
@@ -1,13 +1,17 @@
+import urljoin from 'url-join';
+
 import { PUBLIC_ASSETS_URL } from '../env';
 
+const FACTION_ICONS_PATH = '/faction-icons';
+
 const FACTION_EMBLEMS: { [key: string]: string } = {
-  Polania: `${PUBLIC_ASSETS_URL}/faction-icons/polania.png`,
-  Saxony: `${PUBLIC_ASSETS_URL}/faction-icons/saxony.png`,
-  Crimean: `${PUBLIC_ASSETS_URL}/faction-icons/crimean.png`,
-  Nordic: `${PUBLIC_ASSETS_URL}/faction-icons/nordic.png`,
-  Rusviet: `${PUBLIC_ASSETS_URL}/faction-icons/rusviet.png`,
-  Albion: `${PUBLIC_ASSETS_URL}/faction-icons/albion.png`,
-  Togawa: `${PUBLIC_ASSETS_URL}/faction-icons/togawa.png`
+  Polania: urljoin(PUBLIC_ASSETS_URL, FACTION_ICONS_PATH, 'polania.png'),
+  Saxony: urljoin(PUBLIC_ASSETS_URL, FACTION_ICONS_PATH, 'saxony.png'),
+  Crimean: urljoin(PUBLIC_ASSETS_URL, FACTION_ICONS_PATH, 'crimean.png'),
+  Nordic: urljoin(PUBLIC_ASSETS_URL, FACTION_ICONS_PATH, 'nordic.png'),
+  Rusviet: urljoin(PUBLIC_ASSETS_URL, FACTION_ICONS_PATH, 'rusviet.png'),
+  Albion: urljoin(PUBLIC_ASSETS_URL, FACTION_ICONS_PATH, 'albion.png'),
+  Togawa: urljoin(PUBLIC_ASSETS_URL, FACTION_ICONS_PATH, 'togawa.png')
 };
 
 export const getFactionEmblem = (factionName: string) =>
