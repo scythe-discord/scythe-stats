a0dd42623a821baeed7400e9eafc515d00087c59
Brady Shi <brady-shi@outlook.com>
2020-05-14 14:49:53 -0700

    Move ts-ignore declaration after TS upgrade

--- api/src/graphql/schema/relay.ts
+++ api/src/graphql/schema/relay.ts
@@ -17,13 +17,13 @@ export const typeDef = gql`
 `;
 
 export const resolvers: Schema.Resolvers = {
-  // @ts-ignore: TS complains that the type string may not be a type of
-  // an actual Node, but having this actually happen in practice in unlikely
   Node: {
-    __resolveType: node => {
+    // @ts-ignore: TS complains that the type string may not be a type of
+    // an actual Node, but having this actually happen in practice in unlikely
+    __resolveType: (node) => {
       const { type } = fromGlobalId(node.id);
 
       return type;
-    }
-  }
+    },
+  },
 };
