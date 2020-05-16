fd295b491d5a9418566edf0511bce36861a2c35e
Brady Shi <brady-shi@outlook.com>
2020-05-16 02:08:32 -0700

    Allow _ for unused vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,32 +3,39 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   globals: {
-    module: 'readonly'
+    module: 'readonly',
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:node/recommended-module',
-    'prettier/@typescript-eslint'
+    'prettier/@typescript-eslint',
   ],
   rules: {
     'node/no-missing-import': [
       'error',
       {
-        tryExtensions: ['.js', '.ts', '.tsx', '.json', '.node']
-      }
+        tryExtensions: ['.js', '.ts', '.tsx', '.json', '.node'],
+      },
+    ],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_$',
+      },
     ],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/ban-ts-ignore': 'off'
+    '@typescript-eslint/ban-ts-ignore': 'off',
   },
   overrides: [
     {
       files: ['*.js'],
       rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off'
-      }
-    }
-  ]
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
+  ],
 };
