02b381c059e55930a1b79469ce759b43aa2e7926
Brady Shi <brady-shi@outlook.com>
2020-05-16 07:26:39 -0700

    Fix inconsistently named file

--- api/src/graphql/schema/players/index.ts
+++ api/src/graphql/schema/players/index.ts
@@ -2,12 +2,12 @@ import { merge } from 'lodash';
 
 import {
   typeDef as playerTypeDef,
-  resolvers as playerResolvers
+  resolvers as playerResolvers,
 } from './player';
 import {
   typeDef as playersByWinsTypeDef,
-  resolvers as playersByWinsResolvers
-} from './playersByWins';
+  resolvers as playersByWinsResolvers,
+} from './players-by-wins';
 
 export const typeDef = [playerTypeDef, playersByWinsTypeDef];
 export const resolvers = merge(playerResolvers, playersByWinsResolvers);
