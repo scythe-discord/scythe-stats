834354fd9ccf764bd84303241d2539a499b42688
Brady Shi <brady-shi@outlook.com>
2020-06-12 14:09:54 -0700

    Update TypeORM config to use dotenv

--- api/ormconfig.js
+++ api/ormconfig.js
@@ -1,3 +1,6 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(process.cwd(), '..', '.env') });
+
 module.exports = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
