003fd04da1c979eeee657d1d952d04528573c64c
Brady Shi <brady-shi@outlook.com>
2020-09-20 17:15:22 -0700

    Fix Discord link styling leaking into buttons

--- site/apps/landing-page/discord-link/index.tsx
+++ site/apps/landing-page/discord-link/index.tsx
@@ -1,17 +1,28 @@
-import { withStyle } from 'baseui';
 import { StyledLink } from 'baseui/link';
+import { useStyletron } from 'styletron-react';
 
-export default withStyle(StyledLink as any, () => ({
-  // Some attempt at mimicking Discord blurple
-  color: '#8da0e1',
-  textDecoration: 'none',
-  ':visited': {
-    color: '#8da0e1',
-  },
-  ':hover': {
-    color: '#a8b6e8',
-  },
-  ':active, :focus': {
-    color: '#a8b6e8',
-  },
-}));
+export default (props: any) => {
+  const [css] = useStyletron();
+  return (
+    <StyledLink
+      className={css({
+        // Some attempt at mimicking Discord blurple
+        color: '#8da0e1',
+        textDecoration: 'none',
+        ':visited': {
+          color: '#8da0e1',
+        },
+        ':hover': {
+          color: '#a8b6e8',
+        },
+        ':active': {
+          color: '#a8b6e8',
+        },
+        ':focus': {
+          color: '#a8b6e8',
+        },
+      })}
+      {...props}
+    />
+  );
+};
